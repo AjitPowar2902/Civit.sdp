b93aee2948a8bea40e62c56fe8b1d79d143bd7bc
iamanuragjain <aj.anuragjain14@gmail.com>
2024-08-02 15:56:30 +0530

    chekin login page by anurag

--- src/pages/userdetails/Login.jsx
+++ src/pages/userdetails/Login.jsx
@@ -3,10 +3,13 @@ import "../../styles/Global.scss";
 import { Form, Card, InputGroup, Row, Col,Button } from "react-bootstrap";
 import { MdAlternateEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
-import { FaRegEye } from "react-icons/fa6";
 import { FaRegEyeSlash } from "react-icons/fa6";
-
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
+  const handlenext = (e) => {
+    navigate("/Dashboard");
+  };
   return (
     <>
       <body className="bgLogin-linear" style={{height:'100%'}}>
@@ -61,7 +64,7 @@ export default function Login() {
                   </a>
                 </Col>
                 <Col sm="12" md="12" lg="12" className="mt-3 t-center">
-                  <button className="btn btn-gray w-100"size="lg">
+                  <button className="btn btn-gray w-100"size="lg" onClick={handlenext}>
                   Login
                   </button>
                 </Col>
