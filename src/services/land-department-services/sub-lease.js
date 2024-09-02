bea8b8ac984089f8230200ab519a1471f287a9b1
Ajit Powar <ajitpowar2902@gmail.com>
2024-09-02 12:00:14 +0530

    updated post service for sub-lease

---
+++ src/services/land-department-services/sub-lease.js
@@ -0,0 +1,37 @@
+import CreateAxiosInstance from "../axios-instance";
+import GlobalServiceFile from "../global-services";
+
+const apiService = GlobalServiceFile(
+  CreateAxiosInstance(process.env.REACT_APP_LANDSERVICES_URL)
+);
+
+const POST_SUBLEASE_SERVICE = "/SaveSubLeaseOfPlotService";
+
+export default {
+  postSubLease: async (userId, data) => {
+    const customHeaders = {
+      userId: userId,
+    };
+    try {
+      const response = await apiService.post(
+        POST_SUBLEASE_SERVICE,
+        data,
+        customHeaders
+      );
+
+      if (response.data.IsSuccess === true) {
+        return response.data;
+      } else {
+        console.error("Error response from API:", response.data);
+        return "401";
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 500) {
+        return error.response.status;
+      } else {
+        return error.response.status;
+      }
+      return error.response.status;
+    }
+  },
+};
